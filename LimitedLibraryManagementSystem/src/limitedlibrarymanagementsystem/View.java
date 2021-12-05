0063038121893d35facdffd305cb2397309a372f
Giuliana Bouzon <ga@bouzon.com.br>
2021-12-04 19:43:21 -0500

    fixed returns

--- LimitedLibraryManagementSystem/src/limitedlibrarymanagementsystem/View.java
+++ LimitedLibraryManagementSystem/src/limitedlibrarymanagementsystem/View.java
@@ -32,29 +32,34 @@ import java.util.Map;
  */
 public class View {
 
-    public void printIssuedBookTable(Map<String, String> map) {
-    
+    public String printIssuedBookTable(Map<String, String> map) {
+        String str = "";
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            str += String.format("%-20s : %s\n", "SN", entry.getKey());
+	    str += entry.getValue() + "\n";
+        }
+        return str;
     }
     
-    public void printBookCatalog(Map<String, String> map) {
-	System.out.println("Catalog of Books in the Library: \n");
+    public String printBookCatalog(Map<String, String> map) {
+        String str2 = "";
+        str2 += "Catalog of Books in the Library: \n";
+	//System.out.println("Catalog of Books in the Library: \n");
 	String str = "";
 	
 	for (Map.Entry<String, String> entry : map.entrySet()) {
 	    str += String.format("%-20s : %s\n", "SN", entry.getKey());
 	    str += entry.getValue() + "\n";
 	}
-	
-	System.out.print(str);
+	return str2 + str;
     }
     
-    public void printBookList(List<Book> books) { //just to make it pretty
+    public String printBookList(List<Book> books) { //just to make it pretty
 	String str = "";
 	for (Book book : books) {
 	    str += book.toString();
 	    str += book.getBookData().toString() + "\n";
 	}
-	
-	System.out.print(str);
+	return str;
     }
 }
