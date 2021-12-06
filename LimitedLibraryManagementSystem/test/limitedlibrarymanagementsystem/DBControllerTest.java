7e3550de732fb76a6cf7bfe0785e75f5a0b01bc5
Chilka <chilcjcastro@yahoo.ca>
2021-12-06 16:32:17 -0500

    Added JUnit 5 jar so it would work on netbeans

--- LimitedLibraryManagementSystem/test/limitedlibrarymanagementsystem/DBControllerTest.java
+++ LimitedLibraryManagementSystem/test/limitedlibrarymanagementsystem/DBControllerTest.java
@@ -44,22 +44,6 @@ public class DBControllerTest {
     public DBControllerTest() {
     }
     
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
-    }
-
     /**
      * Test of addBook method, of class DBController.
      */
